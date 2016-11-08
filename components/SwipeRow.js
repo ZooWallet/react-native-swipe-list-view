23b546b0ce2652e2eb367edb5629860655bd27a5
xwartz <stddup@gmail.com>
2016-11-08 18:03:52 +0800

    Fix swipe quickly makes swipe blocked

--- components/SwipeRow.js
+++ components/SwipeRow.js
@@ -48,7 +48,8 @@ class SwipeRow extends Component {
 			onMoveShouldSetPanResponder: (e, gs) => this.handleOnMoveShouldSetPanResponder(e, gs),
 			onPanResponderMove: (e, gs) => this.handlePanResponderMove(e, gs),
 			onPanResponderRelease: (e, gs) => this.handlePanResponderEnd(e, gs),
-			onPanResponderTerminate: (e, gs) => this.handlePanResponderEnd(e, gs),
+			// onPanResponderTerminate: (e, gs) => this.handlePanResponderEnd(e, gs),
+			onPanResponderTerminationRequest: () => false,
 			onShouldBlockNativeResponder: _ => false,
 		});
 	}
